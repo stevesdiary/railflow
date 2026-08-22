7af82e0058ddf9310d690cb95f4a5da661746491
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2026-08-22 21:34:12 +0100

    chore: scaffold pnpm monorepo with tooling configs

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**', '**/*.tsbuildinfo'],
+  },
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' },
+      ],
+    },
+  },
+);
